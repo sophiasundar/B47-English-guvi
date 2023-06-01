29d275c685da8ecdf7e2eada94e4510d4e51fd6c
sophiasundar <sophiagifta17@gmail.com>
2023-06-01 22:11:53 +0530

    commit

--- webcode-1/script.js
+++ webcode-1/script.js
@@ -1,48 +1,47 @@
-// https://pokeapi.co/api/v2/pokemon/${id}
-var data = document.querySelector(".flex-md-row")
-var url= "https://pokeapi.co/api/v2/pokemon/?limit=50&offset=50/${id}"
 
+const cards = document.querySelector(".row")
+
+
+//onclick event 
 async function poke(){
-    const number= document.getElementById('Pokemon').value
-    
-    try{
-    var data = await fetch('url')
-    const res  = await data.json()
-      console.log(res)
-     
-      document.getElementsById("Pokemon").value=""
-     const division= document.createElement('div')
-      division.classList.add('col-lg-4 col-md-6 col-sm-12')
-      division.innerHTML=`<div class="card" style="width: 18rem;">
-      <div class="card-header">
-      <h4> Name:${res.name}</h4>
-      </div>
-      <div class="card-body">
-      <ul class="list-group list-group-flush"><h5>POKEMON details</h5>
-        <li class="list-group-item"><h5>ID-NO:<h6>${res.id}</h6></h5></li>
-        <li class="list-group-item"><h5>ABILITY:<h6>${res.abilities[0].ability.name}</h6></h5></li>
-        <li class="list-group-item"><h5>MOVES:<h6>${res.weight}</h6></h5></li>
-        <li class="list-group-item"><h5>WEIGHT:<h6>${res.moves[0].move.name}</h6></h5></li>
-      </ul>
-      </div>
-    </div>`
-        
-    data.appendchild(division)
-    
-    }
-    catch{
-       alert("Enter upto 50 ID's")
-    }  
-      
-    }
-
+    const number = document.getElementById("num").value
     async function pokedex(){
-      for(var i=1;i<=number;i++){
-         await poke(i)
-      }
+        // User can set number of pokedex
+        for(let i=1; i<=number; i++){         
+            await pokemonapi(i)
+        }
     }
-     pokedex()
 
-     var button= document.querySelector('.btn')
-     button.addEventListener('click',poke)
-
+    //fetching api
+    async function pokemonapi(id){
+        try {
+            const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}?limit=50&offset=50`);
+            const res = await data.json();
+            console.log(res); 
+            
+            //o/p
+            document.getElementById("num").value = "";
+            const div = document.createElement('div');
+            div.className="col-lg-4 col-md-6 col-sm-12"
+            div.innerHTML=`
+            <div class="card" style="width: 18rem;">
+            <div class="card-header">
+            <h5><strong>Name:</strong> ${res.name}</h5>
+            </div>
+            
+            <div class="card-body" id="pokemon-info">
+                <h6><strong>Number: </strong> ${res.id} </h6>
+                <h6><strong>Ability: </strong> ${res.abilities[0].ability.name} </h6>
+                <h6><strong>Moves: </strong> ${res.moves[0].move.name} </h6>
+                <h6><strong>Weight: </strong> ${res.weight} </h6>
+            </div>
+            </div>
+            `
+            cards.appendChild(div)
+        } catch (error) {
+            alert("Refresh the page")
+        }
+    }
+    
+    pokedex();
+}
