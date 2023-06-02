2874eb027ce41709754133d339c45012ae377c24
sophiasundar <sophiagifta17@gmail.com>
2023-06-02 22:49:47 +0530

    commit

--- day-21-recap/TASK-Holiday/script.js
+++
@@ -1,6 +0,0 @@
-//  curl -G -d country="IN" -d year="2023" -d pretty
-// -d key="25764c12-e9a0-4f15-82a0-cbb32a3bbb98" "https://holidayapi.com/v1/holidays"
-
-let input = document.querySelector('.container')
-
-input.addEventListener()
